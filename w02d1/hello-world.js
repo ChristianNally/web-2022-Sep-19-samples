989518d12cafba32a55981d03ca08536dcf08d37
Christian Nally <sticksallison@gmail.com>
2022-09-26 11:33:07 -0700

    in-class edits

--- w02d1/hello-world.js
+++ w02d1/hello-world.js
@@ -1,7 +1,16 @@
 // implement a function called sayHello that takes the name of the saluted and returns what would be said.
-const sayHello = () => {
+const sayHello = (name) => {
+  const output = `Hello, ${name}`;
+  return output;
 };
 
-// show some test code here at first, and then factor it out later
+const sayGoodbye = (name) => {
+  const output = `Goodbye, ${name}`;
+  return output;
+};
 
 // make the functions exportable
+module.exports = {
+  sayHello: sayHello,
+  sayGoodbye: sayGoodbye
+};
--- w02d1/test/hello-world.test.js
+++ w02d1/test/hello-world.test.js
@@ -1,8 +1,41 @@
 // pull in whichever assert library you'd like to use
-
+const assert = require('assert');
 
 // pull in the main functions that will be tested
-
+const functionCollection = require('../hello-world.js');
+const sayHello = functionCollection.sayHello;
+const sayGoodbye = functionCollection.sayGoodbye;
 
 // write the mocha describe() and it() function calls that will implement the test(s)
 
+describe('sayHello Tests', () => {
+
+  it('returns the expected salutation from sayHello', () => {
+    const expected = 'Hello, World';
+    const actual = sayHello('World');
+    assert.strictEqual(actual, expected);  
+  });
+  
+  it('returns the expected salutation from sayHello for a second example', () => {
+    const expected = 'Hello, World!!';
+    const actual = sayHello('World!!');
+    assert.strictEqual(actual, expected);  
+  });  
+
+});
+
+describe('sayGoodbye Tests', () => {
+
+  it('returns the expected salutation from sayGoodbye', () => {
+    const expected = 'Goodbye, World';
+    const actual = sayGoodbye('World');
+    assert.strictEqual(actual, expected);  
+  });
+  
+  it('returns the expected salutation from sayGoodbye for a second example', () => {
+    const expected = 'Goodbye, World!!';
+    const actual = sayGoodbye('World!!');
+    assert.strictEqual(actual, expected);  
+  });  
+
+});
